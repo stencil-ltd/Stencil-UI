c84cde35744bee4d49514ff7bb13e5a19a4de83c
Aaron Sarazan <aaron@sarazan.net>
2018-10-20 12:19:51 -0700

    registerable

---
+++ Scripts/RegisterableBehaviour.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace UI
+{
+    public abstract class RegisterableBehaviour : MonoBehaviour
+    {
+        public bool Registered { get; internal set; }
+        public bool Unregistered { get; internal set; }
+        
+        public virtual void Register() {}
+        public virtual void DidRegister() {}
+        
+        public virtual void Unregister() {}        
+        public virtual void WillUnregister() {}
+    }
+}
