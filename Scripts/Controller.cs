cdd617341fd70646e97d3375e9f12e7d548ba48e
Aaron Sarazan <aaron@sarazan.net>
2018-10-21 15:33:51 -0700

    move controller to ui

---
+++ Scripts/Controller.cs
@@ -0,0 +1,27 @@
+﻿namespace UI
+{
+    public abstract class Controller<T> : RegisterableBehaviour where T : Controller<T>
+    {
+        public static T Instance { get; private set; }
+
+        private void Awake()
+        {
+            Instance = (T)this;
+        }
+
+        private void OnEnable()
+        {
+            Instance = (T)this;
+        }
+
+        public override void Register()
+        {
+            Instance = (T)this;
+        }
+
+        public override void Unregister()
+        {
+            Instance = Instance == this ? null : Instance;
+        }
+    }
+}
