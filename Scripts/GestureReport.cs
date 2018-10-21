2208f78f5267974610dbad1e6df531da8c6f1426
Aaron Sarazan <aaron@sarazan.net>
2018-10-21 07:15:41 -0700

    move gesture stuff over

---
+++ Scripts/GestureReport.cs
@@ -0,0 +1,25 @@
+﻿using System.IO;
+using UnityEngine;
+
+namespace UI
+{
+    [RequireComponent(typeof(Gestures))]
+    public class GestureReport : MonoBehaviour
+    {
+        private void Awake()
+        {
+            LogCollector.Init();
+            GetComponent<Gestures>().OnGesture += (sender, args) => Report();
+        }
+        
+        private void Report()
+        {
+            var str = LogCollector.GetLogString();
+            var path = Application.temporaryCachePath + "/logs.txt";
+            File.WriteAllText(path, str);
+            if (!Application.isEditor)
+                NativeShare.Share("", path, mimeType: "text/plain", chooser: true);
+            else Debug.Log($"Share: {path}");      
+        }
+    }
+}
