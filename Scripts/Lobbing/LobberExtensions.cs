da2ea015a3d88fe292d2b320ce20b220d42c71f6
Aaron Sarazan <aaron@sarazan.net>
2018-11-19 17:25:17 -0800

    progress on end screen

--- Scripts/Lobbing/LobberExtensions.cs
+++ Scripts/Lobbing/LobberExtensions.cs
@@ -6,18 +6,32 @@ namespace Lobbing
 {
     public static class LobberExtensions
     {
-        public static IEnumerator LobCurrency(this Lobber lobber, Currency currency, long amount, LobOverrides overrides = null)
+        public class LobOptions
         {
-            currency.Stage(amount).AndSave();
-            overrides = overrides ?? new LobOverrides();
+            public bool PreStaged;
+            public LobOverrides Overrides;
+
+            public static implicit operator LobOverrides(LobOptions options) => options.Overrides;
+            public static implicit operator LobOptions(LobOverrides overrides) => new LobOptions
+            {
+                Overrides = overrides
+            };
+        }
+        
+        public static IEnumerator LobCurrency(this Lobber lobber, Currency currency, long amount, LobOptions options = null)
+        {
+            if (options?.PreStaged != true)
+                currency.Stage(amount).AndSave();
+            var overrides = options?.Overrides ?? new LobOverrides();
             overrides.OnEachComplete += (sender, lob) => currency.Commit(lob.Amount);
             yield return Objects.StartCoroutine(lobber.LobMany(amount, overrides));
         }
         
-        public static IEnumerator LobCurrencySingle(this Lobber lobber, Currency currency, long amount, LobOverrides overrides = null)
+        public static IEnumerator LobCurrencySingle(this Lobber lobber, Currency currency, long amount, LobOptions options = null)
         {
-            currency.Stage(amount).AndSave();
-            overrides = overrides ?? new LobOverrides();
+            if (options?.PreStaged != true)
+                currency.Stage(amount).AndSave();
+            var overrides = options?.Overrides ?? new LobOverrides();
             overrides.OnEachComplete += (sender, lob) => currency.Commit(lob.Amount);
             yield return Objects.StartCoroutine(lobber.LobSingle(amount, false, overrides));
         }
