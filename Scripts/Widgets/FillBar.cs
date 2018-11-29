2520a82e9b70e003b29c7fc1b5c30a21240f60be
Aaron Sarazan <aaron@sarazan.net>
2018-11-29 09:44:26 -0800

    Fill Bar

---
+++ Scripts/Widgets/FillBar.cs
@@ -0,0 +1,45 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Widgets
+{
+    public class FillBar : MonoBehaviour
+    {
+        [Header("Data")]
+        public float amount = 0.5f;
+        public float max = 1f;
+        
+        [Header("UI")]
+        public Image track;
+        public Image fill;
+        public Text text;
+
+        [Header("Config")] 
+        public string textFormat = "{0}";
+        public float smoothing = 5f;
+
+        public float CurrentAmount { get; private set; }
+        public float CurrentNorm => Mathf.Clamp(CurrentAmount / max, 0, 1);
+        
+        private void Awake()
+        {
+            CurrentAmount = amount;
+        }
+        
+        private void Update()
+        {
+            if (!enabled) return;
+            var smooth = Application.isPlaying ? Time.deltaTime * smoothing : 1f;
+            CurrentAmount = Mathf.Lerp(CurrentAmount, amount, smooth);
+            var norm = CurrentNorm;
+            fill.fillAmount = norm;
+            text.text = string.Format(textFormat, amount);
+        }
+
+        public void SetAmounts(int amount, int max)
+        {
+            this.amount = CurrentAmount = amount;
+            this.max = max;
+        }
+    }
+}
