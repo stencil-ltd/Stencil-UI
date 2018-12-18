3d288de2de01236300c529171cd66e0349505488
Aaron Sarazan <aaron@sarazan.net>
2018-12-18 13:43:27 -0800

    more anim hack

--- Scripts/Util/RestartAnimator.cs
+++ Scripts/Util/RestartAnimator.cs
@@ -6,13 +6,17 @@ namespace Util
 {
     public class RestartAnimator : MonoBehaviour
     {
+        public string trigger = "Normal";
+        
         [Bind]
         public Animator Animator { get; private set; }
         
         private void OnEnable()
         {
             this.Bind();
-            StartCoroutine(_DoIt());
+            Animator.SetTrigger(trigger);
+//            Animator.Rebind();
+//            StartCoroutine(_DoIt());
         }
 
         private IEnumerator _DoIt()
