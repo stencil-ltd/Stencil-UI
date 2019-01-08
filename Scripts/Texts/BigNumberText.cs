76997b7e3ada13adac051ecb1bcae612064b3527
Aaron Sarazan <aaron@sarazan.net>
2019-01-07 17:50:07 -0800

    big number tet fix

--- Scripts/Texts/BigNumberText.cs
+++ Scripts/Texts/BigNumberText.cs
@@ -1,8 +1,11 @@
+using System;
+using System.Collections;
 using Binding;
 using Dirichlet.Numerics;
 using Scripts.Util;
 using UnityEngine;
 using UnityEngine.UI;
+using Util;
 
 namespace Texts
 {
@@ -11,21 +14,64 @@ namespace Texts
     {
         [Header("UI")] 
         public NumberFormats.Format format = NumberFormats.Format.MassiveAmount;
+        public AnimationCurve animCurve = Curves.EaseInOut;
         public string prefix = "";
-        
+
+        public UInt128 Amount { get; private set; }
+
         [Bind]
         private Text _text;
-        
-        public UInt128 Amount { get; private set; }
+
+        private Coroutine _co;
 
         private void Awake()
         {
             this.Bind();
         }
 
-        private void Update()
+        private void OnEnable()
+        {
+            Refresh();
+        }
+
+        private void LateUpdate()
+        {
+            Refresh();
+        }
+
+        private void Refresh()
         {
             _text.text = prefix + format.FormatAmount(Amount);
+        }
+
+        public void Set(UInt128 amount)
+        {
+            if (_co != null)
+            {
+                StopCoroutine(_co);
+                _co = null;
+            }
+            Amount = amount;
+        }
+
+        public void Lerp(UInt128 amount, float time = 1f)
+        {
+            if (amount == Amount) return;
+            if (_co != null) StopCoroutine(_co);
+            _co = StartCoroutine(_Lerp(amount, time));
+        }
+
+        private IEnumerator _Lerp(UInt128 amount, float time)
+        {
+            var from = Amount;
+            var start = DateTime.UtcNow;
+            for (;;)
+            {
+                var elapsed = (float) (DateTime.UtcNow - start).TotalSeconds;
+                var norm = animCurve.Evaluate(elapsed / time);
+                Amount = UInt128.Lerp(from, amount, norm);
+                yield return null;
+            }
         }
     }
 }
