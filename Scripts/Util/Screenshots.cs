b2ac6758e2532f0679e65e7bf551fd9807e230df
Aaron Sarazan <aaron@sarazan.net>
2019-07-07 15:36:42 -0700

    screenshot helpers

---
+++ Scripts/Util/Screenshots.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using JetBrains.Annotations;
+using UnityEngine;
+
+namespace Util.UI
+{
+    public static class Screenshots
+    {
+        public static IEnumerator TakeScreenshot(this RectTransform transform, Action<Texture2D> callback)
+        {
+            var screenRect = transform.ToScreenSpace();
+            var tex = new Texture2D((int) screenRect.width, (int) screenRect.height, TextureFormat.ARGB32, false);
+            yield return new WaitForEndOfFrame();
+            tex.ReadPixels(screenRect, 0, 0);
+            tex.Apply();
+            callback.Invoke(tex);
+            yield return null;
+        }
+
+        public static IEnumerator ShareScreenshotSimple([CanBeNull] string title = null)
+        {
+            yield return new WaitForEndOfFrame();
+            var path = "screenshot.png";
+            ScreenCapture.CaptureScreenshot(path);
+            new NativeShare()
+                .SetTitle(title)
+                .AddFile($"{Application.persistentDataPath}/{path}")
+                .Share();
+        }
+    }
+}
