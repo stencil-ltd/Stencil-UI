9f3a97c6237989e7b6134232ea0de2c5f2ff272b
Aaron Sarazan <aaron@sarazan.net>
2019-07-22 18:24:01 -0700

    state buttons can rotate through state options

--- Scripts/State/StateButton.cs
+++ Scripts/State/StateButton.cs
@@ -1,3 +1,4 @@
+using System;
 using Binding;
 using UnityEngine;
 using UnityEngine.UI;
@@ -11,6 +12,7 @@ namespace State
         public T state;
         public bool popStateInstead;
         public bool replaceHistory;
+        public bool rotateInstead;
         
         private StateMachine<T> _machine;
 
@@ -20,9 +22,21 @@ namespace State
             GetComponent<Button>().onClick.AddListener(() =>
             {
                 if (popStateInstead)
+                {
                     _machine.PopState();
-                else 
+                }
+                else if (rotateInstead)
+                {
+                    var all = (T[]) Enum.GetValues(typeof(T));
+                    var current = (int) (object) _machine.State;
+                    current = (current + 1) % all.Length;
+                    _machine.RequestState((T) (object) current, replaceHistory: replaceHistory);
+                }
+                else
+                {
                     _machine.RequestState(state, replaceHistory: replaceHistory);
+                }
+                    
             });
         }
     }
