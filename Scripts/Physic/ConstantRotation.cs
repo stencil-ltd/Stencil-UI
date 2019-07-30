2a03a3fc0afa7c620e3e0dfdcddb1342eed8e7c9
Aaron Sarazan <aaron@sarazan.net>
2019-07-30 13:27:06 -0700

    ConstantRotation and friends

---
+++ Scripts/Physic/ConstantRotation.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+namespace Physic
+{
+    [ExecuteInEditMode]
+    public class ConstantRotation : MonoBehaviour
+    {
+        public bool RunInEditor;
+        
+        public Vector3 Velocity;
+        private Quaternion _rotation;
+
+        private void OnEnable()
+        {
+            _rotation = transform.localRotation;
+        }
+
+        private void OnDisable()
+        {
+            transform.localRotation = _rotation;
+        }
+
+        private void Update()
+        {
+            if (Application.isPlaying || RunInEditor)
+            transform.Rotate(Velocity * Time.deltaTime);
+        }
+    }
+}
---
+++ Scripts/Physic/ConstantVelocity.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+namespace Physic
+{
+    public class ConstantVelocity : MonoBehaviour
+    {
+        public Vector3 Velocity;
+
+        public float x
+        {
+            get => Velocity.x;
+            set => Velocity.x = value;
+        }
+
+        public float y
+        {
+            get => Velocity.y;
+            set => Velocity.y = value;
+        }
+
+        public float z
+        {
+            get => Velocity.z;
+            set => Velocity.z = value;
+        }
+
+        private void Update()
+        {
+            var pos = transform.position;
+            pos += Velocity * Time.deltaTime;
+            transform.position = pos;
+        }
+    }
+}
