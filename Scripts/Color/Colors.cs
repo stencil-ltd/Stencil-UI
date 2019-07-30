36d9acdc4e18913df22dd26094c4571615112fea
Aaron Sarazan <aaron@sarazan.net>
2019-07-30 10:36:27 -0700

    Move color extensions

---
+++ Scripts/Color/Colors.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Plugins.Util
+{
+    public static class Colors
+    {
+        public static string LogString(this Color color)
+        {
+            return $"#{(byte)(color.r * 255):X2}{(byte)(color.g * 255):X2}{(byte)(color.b * 255):X2}";
+        }
+
+        public static Color Alpha(this Image image, float alpha)
+        {
+            var color = image.color;
+            color.a = alpha;
+            image.color = color;
+            return color;
+        }
+    }
+}
