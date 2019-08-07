469d3e0a608411d499b63f01a6dab1aa0ee7fd3c
Aaron Sarazan <aaron@sarazan.net>
2019-08-06 17:47:48 -0700

    texture extensions

--- Scripts/Texture/TextureExtensions.cs
+++ Scripts/Texture/TextureExtensions.cs
@@ -1,12 +1,20 @@
+using JetBrains.Annotations;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace Scripts.Texture
 {
     public static class TextureExtensions
     {
-        public static Sprite ToSprite(this Texture2D tex)
+        public static Sprite ToSprite([CanBeNull] this Texture2D tex)
         {
+            if (tex == null) return null;
             return Sprite.Create(tex, new Rect(0, 0, tex.width, tex.height), new Vector2(0.5f, 0.5f));
+        }
+
+        public static void SetTexture(this Image image, Texture2D tex)
+        {
+            image.sprite = tex.ToSprite();
         }
     }
 }
