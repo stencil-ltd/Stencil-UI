7a97568047e94bac80f04e638f21addd8ee22b9b
Aaron Sarazan <aaron@sarazan.net>
2019-12-03 14:23:54 -0800

    right video stuff sucks

--- Scripts/Video/InvertedTextureHacks.cs
+++ Scripts/Video/InvertedTextureHacks.cs
@@ -12,18 +12,43 @@ namespace Scripts.Texture
         // Not idempotent. Please only call once. Results not guaranteed.
         public static void InvertedVideoHack(this RawImage image)
         {
-            #if UNITY_EDITOR || !UNITY_ANDROID
-            return;
-            #endif
-            var build = new AndroidJavaClass("android.os.Build");
-            var mfg = build.GetStatic<string>("MANUFACTURER").ToLower().Trim();
-            if (mfg == "google")
+            if (ShouldApplyVideoHack())
             {
                 var rect = image.rectTransform;
                 var scale = rect.localScale;
                 scale.y = -scale.y;
                 rect.localScale = scale;
             }
+        }
+
+        public static void InvertedVideoHack2(this RawImage image, bool mirror = false)
+        {
+            var rect = new Rect(mirror ? 1 : 0, 0, mirror ? -1 : 1, 1);
+            if (ShouldApplyVideoHack())
+            {
+                rect.y = 1;
+                rect.height = -1;
+            }
+            image.uvRect = rect;
+        }
+        
+        public static void InvertedVideoHack3(this RawImage image, bool mirror = false)
+        {
+            var scale = image.transform.localScale;
+            if (mirror) scale.x *= -1;
+            if (ShouldApplyVideoHack()) scale.y *= -1;
+            image.transform.localScale = scale;
+        }
+
+        public static bool ShouldApplyVideoHack()
+        {
+            return true;
+#if UNITY_EDITOR || !UNITY_ANDROID
+            return false;
+#endif
+            var build = new AndroidJavaClass("android.os.Build");
+            var mfg = build.GetStatic<string>("MANUFACTURER").ToLower().Trim();
+            return mfg == "google";
         }
     }
 }
